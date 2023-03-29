f047f6d67659ddc5ff70546c082110a31c104202
Lucas Teixeira <prilosac@gmail.com>
2023-03-28 22:52:10 -0700

    fiddling around, introduce react router for client side routing, embedded ASS doc, linked to slippi rankings

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import "./Home.css";
+function Home() {
+  return (
+    <div style={{marginTop:2+'em'}}>
+      <iframe width="960" height="540" src="https://www.youtube-nocookie.com/embed/04jXEYYSLy0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </div>
+  );
+}
+
+export default Home;
