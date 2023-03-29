50f49d248fe2f589c99b64d48c02c9e57350645a
Jason Parker <Jbparker243@gmail.com>
2023-03-29 12:51:52 -0700

    added ranking images to PowerRankings component. need to fix the typed out ranking styling

--- src/components/PowerRankings.js
+++ src/components/PowerRankings.js
@@ -1,10 +1,22 @@
 import "./PowerRankings.css";
-function PowerRankings() {
+import { RANKINGS } from "./Rankings";
+const PowerRankings = () => {
   return (
-    <div style={{marginTop:2+'em'}}>
+    <div style={{ marginTop: 2 + "em" }}>
       <h2>Power Rankings</h2>
+      <div className="products">
+        {" "}
+        {RANKINGS.map((ranking) => (
+          <div>
+            <p>{ranking.region}</p>
+            <img src={ranking.image} alt="rankingImages" />
+            <p>{ranking.typedOut}</p>
+            <div className="description"></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default PowerRankings;
---
+++ src/components/Rankings.js
@@ -0,0 +1,27 @@
+import oregonMelee from "./rankingImages/oregonPR.png";
+import pdx from "./rankingImages/pdxWinter2022.png";
+import eugeneCorvo from "./rankingImages/EugCorvoPRW23.png";
+
+export const RANKINGS = [
+  {
+    id: 1,
+    region: "Oregon",
+    image: oregonMelee,
+    typedOut:
+      " 1. Fatgoku 2. Stiv 3. DeCoste 4. Yamasaki 5. God's Only Son 6. Jiffy 7. Yeehaw Daniels 8. Hurricane 9. Dr. Z 10. Nox",
+  },
+  {
+    id: 2,
+    region: "Portland",
+    image: pdx,
+    typedOut:
+      " 1. Fatgoku 2. Stiv 3. Yamasaki 4. Yeehaw Daniels 5. Burlap 6. Naxy 7. ErodingSun 8. Keppler 9. Nox 10. Yalli HM: Cat Named V, Myke B, Dr. Z",
+  },
+  {
+    id: 3,
+    region: "Eugene/Corvo",
+    image: eugeneCorvo,
+    typedOut:
+      " 1. Jiffy 2. Cujo 3. Hurricane 4. Mitchi 5. NordVPN 6. Selise 7. Wenis Tickler 8. Dr. Freashly 9. Froggy 10. Tok3",
+  },
+];
